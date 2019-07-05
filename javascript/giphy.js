43a3c0f7fed52f193bab888462e51f6ea3e5a789
Eric Ballesteros <ballesteros@icloud.com>
2019-07-05 11:39:12 -0500

    completed javascript and styling

--- javascript/giphy.js
+++ javascript/giphy.js
@@ -4,6 +4,7 @@
 // Create buttons from array 
 // Allow new entries to be added to array 
 // Allow new entries to generate a new button
+// For centering divs, buttons, use index.html and style="text-align:center;"
 
 // Giphy API Key:  WxPRwzSdMXDIOYktHQsS6bmcpCpBnfVb
 
@@ -17,6 +18,7 @@ var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey;
 
 
 function giphyResults (numResults,queryURL) {
+    console.log(numResults);
     $.ajax({
         url: queryURL,
         method: "GET"
@@ -27,7 +29,6 @@ function giphyResults (numResults,queryURL) {
                 var imgURL = response.data[i].images.original_still.url;
                 //console.log(imgURL);
                 //console.log(response.data[i].images.original_still.url);
-            
                 var imageArea = $('<div>');
                 imageArea.addClass("well");
                 imageArea.attr('id', 'imageNum-' + i);
@@ -38,9 +39,9 @@ function giphyResults (numResults,queryURL) {
             }
         })
     }
-
 giphyResults ();
 
+//take user search value and create new API request, run results function
 $("#searchbtn").on('click',function() {
     console.log("searchingtesting");
     primaryGenre = $("#genre-input").val().trim();
@@ -49,7 +50,6 @@ $("#searchbtn").on('click',function() {
         console.log(newURL);
     giphyResults(numResults,newURL);
     return false;
-
 });
 
 //make the buttons
@@ -61,47 +61,29 @@ function buttonMaker() {
     genresButtons.addClass("genre-button-color genre-button genres");
     genresButtons.attr("data-name", genres[i]);
     genresButtons.attr('id', 'genArrayNum-' + i);
+    genresButtons.attr('id', 'testbutton');
     genresButtons.text(genres[i]);
+    genresButtons.val(genres[i]);
     }
 }
-
 buttonMaker() ;
 
-//add a button 
+//add a button after search
+//creation of the button inits request to API
 $("#searchbtn").on("click", function(event) {
     event.preventDefault();
     var addedGenre = $("#genre-input").val().trim();
     genres.push(addedGenre);
     buttonMaker();
     console.log(genres);
-
-    //creation of the button inits request to API
-    //giphyResults();
-
 });
 
-//show results of user entry
-
-$(".genre-button").on('click', function(event) {
+//Took a long time to figure out how to point to the value and figure out how to use delegate,
+$("#buttonArea").on('click', ".genre-button", function(event) {
     event.preventDefault();
-    $("#imageArea").empty();
-    console.log(event.data);
-    secondaryGenre = $(".data").val();
+    secondaryGenre = $(this).text();
     console.log(secondaryGenre);
     var newtwoURL = queryURLBase + "&q=" + secondaryGenre + "-movie&limit=10&offset=0&rating=PG-13&lang=en"
     console.log(newtwoURL);
     giphyResults(numResults,newtwoURL);
-return false;
 });
-
-//function buttonAction () {
-    //$(".genre-button").on("click", function(event) {
-        //event.preventDefault();
-        //$(".imageArea").empty();
-
-        //giphyResults();
-    //})}
-
-//buttonAction (); 
-
-
