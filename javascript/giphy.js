38631c8ae9b0a37dae3610f7c43dbd31b6df4dc6
Eric Ballesteros <ballesteros@icloud.com>
2019-07-04 23:29:06 -0500

    updated javascript, nearly  complete

---
+++ javascript/giphy.js
@@ -0,0 +1,107 @@
+// Request with Giphy API 
+// Create an array of thematic topics 
+// Build a form/input 
+// Create buttons from array 
+// Allow new entries to be added to array 
+// Allow new entries to generate a new button
+
+// Giphy API Key:  WxPRwzSdMXDIOYktHQsS6bmcpCpBnfVb
+
+var genres = ["Action", "Anime", "Comedy", "Crime", "Kids", "Racing", "Spooky", "Silly", "Thriller", "War"]
+console.log(genres);
+
+var apiKey = "WxPRwzSdMXDIOYktHQsS6bmcpCpBnfVb";
+var numResults = 10;
+var primaryGenre = ""
+var queryURLBase = "https://api.giphy.com/v1/gifs/search?api_key=" + apiKey;
+
+
+function giphyResults (numResults,queryURL) {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        .done(function(response) {
+            $("#imageArea").empty();
+            for (var i = 0; i <response.data.length; i++) {
+                var imgURL = response.data[i].images.original_still.url;
+                //console.log(imgURL);
+                //console.log(response.data[i].images.original_still.url);
+            
+                var imageArea = $('<div>');
+                imageArea.addClass("well");
+                imageArea.attr('id', 'imageNum-' + i);
+                $('#imageArea').append(imageArea);
+                $("#imageNum-" + i).append(image);
+                var image = $("<img>").attr("src", imgURL);
+                $(".imageArea").append(image);
+            }
+        })
+    }
+
+giphyResults ();
+
+$("#searchbtn").on('click',function() {
+    console.log("searchingtesting");
+    primaryGenre = $("#genre-input").val().trim();
+        console.log(primaryGenre);
+    var newURL = queryURLBase + "&q=" + primaryGenre + "-movie&limit=10&offset=0&rating=PG-13&lang=en"
+        console.log(newURL);
+    giphyResults(numResults,newURL);
+    return false;
+
+});
+
+//make the buttons
+function buttonMaker() {
+    $("#buttonArea").empty();
+    for(var i = 0; i < genres.length; i++) {
+    var genresButtons = $("<button>");
+    $("#buttonArea").append(genresButtons);
+    genresButtons.addClass("genre-button-color genre-button genres");
+    genresButtons.attr("data-name", genres[i]);
+    genresButtons.attr('id', 'genArrayNum-' + i);
+    genresButtons.text(genres[i]);
+    }
+}
+
+buttonMaker() ;
+
+//add a button 
+$("#searchbtn").on("click", function(event) {
+    event.preventDefault();
+    var addedGenre = $("#genre-input").val().trim();
+    genres.push(addedGenre);
+    buttonMaker();
+    console.log(genres);
+
+    //creation of the button inits request to API
+    //giphyResults();
+
+});
+
+//show results of user entry
+
+$(".genre-button").on('click', function(event) {
+    event.preventDefault();
+    $("#imageArea").empty();
+    console.log(event.data);
+    secondaryGenre = $(".data").val();
+    console.log(secondaryGenre);
+    var newtwoURL = queryURLBase + "&q=" + secondaryGenre + "-movie&limit=10&offset=0&rating=PG-13&lang=en"
+    console.log(newtwoURL);
+    giphyResults(numResults,newtwoURL);
+return false;
+});
+
+//function buttonAction () {
+    //$(".genre-button").on("click", function(event) {
+        //event.preventDefault();
+        //$(".imageArea").empty();
+
+        //giphyResults();
+    //})}
+
+//buttonAction (); 
+
+
